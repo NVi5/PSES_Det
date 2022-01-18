7fa9929e161738e9673006b9e16660394a3e9935
Jan Wołowiec <wolowiec.jan7@gmail.com>
2022-01-18 23:42:49 +0100

    Update hooks

--- inc/Det_Externals.h
+++ inc/Det_Externals.h
@@ -10,21 +10,105 @@
 #include "Std_Types.h"
 
 /**
-    @brief Service to initialize the Default Error Tracer.
-    @req [SWS_Det_00008]
+    @brief Error_Hook to be called by Det_ReportError.
+    @req [SWS_Det_00181]
 
-    @param ConfigPtr Pointer to the selected configuration set.
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
+
+    @returns returns always E_OK (is required for services)
 */
-Std_ReturnType MyErrorHook(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+Std_ReturnType MyErrorHook(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
 
-Std_ReturnType MyErrorHook2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+/**
+    @brief Error_Hook to be called by Det_ReportError.
+    @req [SWS_Det_00181]
 
-Std_ReturnType MyRuntimeErrorCallout(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
 
-Std_ReturnType MyRuntimeErrorCallout2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+    @returns returns always E_OK (is required for services)
+*/
+Std_ReturnType MyErrorHook2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
 
-Std_ReturnType MyTransientFaultCallout(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+/**
+    @brief Error_Hook to be called by Det_ReportRuntimeError.
+    @req [SWS_Det_00184]
 
-Std_ReturnType MyTransientFaultCallout2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId);
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
+
+    @returns returns always E_OK (is required for services)
+*/
+Std_ReturnType MyRuntimeErrorCallout(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
+
+/**
+    @brief Error_Hook to be called by Det_ReportRuntimeError.
+    @req [SWS_Det_00184]
+
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
+
+    @returns returns always E_OK (is required for services)
+*/
+Std_ReturnType MyRuntimeErrorCallout2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
+
+/**
+    @brief Error_Hook to be called by Det_ReportTransientFault.
+    @req [SWS_Det_00187]
+
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
+
+    @returns Value is propagated to caller of Det_ReportTransientFault
+*/
+Std_ReturnType MyTransientFaultCallout(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
+
+/**
+    @brief Error_Hook to be called by Det_ReportTransientFault.
+    @req [SWS_Det_00187]
+
+    @param ModuleId Module ID of calling module.
+    @param InstanceId The identifier of the index based instance of a module, starting
+    from 0, If the module is a single instance module it shall pass 0 as
+    the InstanceId.
+    @param ApiId ID of API service in which error is detected (defined in SWS of
+    calling module)
+    @param ErrorId ID of detected development error (defined in SWS of calling
+    module).
+
+    @returns Value is propagated to caller of Det_ReportTransientFault
+*/
+Std_ReturnType MyTransientFaultCallout2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId);
 
 #endif /* DET_EXTERNALS */
--- src/Det_User.c
+++ src/Det_User.c
@@ -6,32 +6,38 @@
 #include "Det_Externals.h"
 #include <stdio.h>
 
-Std_ReturnType MyErrorHook(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyErrorHook(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Error: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
 
-Std_ReturnType MyErrorHook2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyErrorHook2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Error 2: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
 
-Std_ReturnType MyRuntimeErrorCallout(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyRuntimeErrorCallout(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Runtime Error Callout: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
 
-Std_ReturnType MyRuntimeErrorCallout2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyRuntimeErrorCallout2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Runtime Error Callout 2: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
 
-Std_ReturnType MyTransientFaultCallout(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyTransientFaultCallout(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Transient Fault Callout: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
 
-Std_ReturnType MyTransientFaultCallout2(uint16 ModuleId, uint8 InstanceId , uint8 ApiId, uint8 ErrorId) {
+Std_ReturnType MyTransientFaultCallout2(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {
     printf("Reported Transient Fault Callout 2: ModuleId:%d, InstanceId:%d, ApiId:%d, ErrorId:%d",
             ModuleId, InstanceId, ApiId, ErrorId);
+    return E_OK;
 }
